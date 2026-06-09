ce90dfd0000d37ca9b5a36e0e15d4af3f23edcdb
Sebanev15 <sneves@impulso.edu.uy>
2026-06-09 17:50:15 -0300

    add coverage to puntosDeArticulacion

--- src/test/java/ucu/edu/aed/UndirectedGraphAlgorithmsTest.java
+++ src/test/java/ucu/edu/aed/UndirectedGraphAlgorithmsTest.java
@@ -10,6 +10,93 @@ public class UndirectedGraphAlgorithmsTest {
 
     @Test
     public void puntosArticulacionInEmptyGraphTest(){
-        algorithms.puntosDeArticulacion()
+        assertTrue(algorithms.puntosDeArticulacion(g).isEmpty());
+    }
+
+    @Test
+    public void puntosArticulacionInSingleVertexGraphTest(){
+        g.agregarVertice("A");
+        assertTrue(algorithms.puntosDeArticulacion(g).isEmpty());
+    }
+
+    @Test
+    public void puntosArticulacionInTwoVerticesGraphTest(){
+        g.agregarVertice("A");
+        g.agregarVertice("B");
+        assertTrue(algorithms.puntosDeArticulacion(g).isEmpty());
+    }
+
+    @Test
+    public void puntosArticulacionInThreeVerticesGraphTest() {
+        g.agregarVertice("A");
+        g.agregarVertice("B");
+        g.agregarVertice("C");
+        g.agregarArista("A", "B", 1);
+        g.agregarArista("B", "C", 1);
+        assertEquals(1, algorithms.puntosDeArticulacion(g).size());
+        assertTrue(algorithms.puntosDeArticulacion(g).contains("B"));
+    }
+
+    @Test
+    public void puntosArticulacionInLargeGraphTest() {
+        g.agregarVertice("A");
+        g.agregarVertice("B");
+        g.agregarVertice("C");
+        g.agregarVertice("D");
+        g.agregarVertice("E");
+        g.agregarArista("A", "B", 1);
+        g.agregarArista("B", "C", 1);
+        g.agregarArista("C", "D", 1);
+        g.agregarArista("D", "E", 1);
+
+        assertEquals(3, algorithms.puntosDeArticulacion(g).size());
+        assertTrue(algorithms.puntosDeArticulacion(g).contains("B"));
+        assertTrue(algorithms.puntosDeArticulacion(g).contains("C"));
+        assertTrue(algorithms.puntosDeArticulacion(g).contains("D"));
+    }
+
+    @Test
+    public void puntosArticulacionInCiclicGraphTest(){
+        g.agregarVertice("A");
+        g.agregarVertice("B");
+        g.agregarVertice("C");
+        g.agregarVertice("D");
+        g.agregarArista("A", "B", 1);
+        g.agregarArista("B", "C", 1);
+        g.agregarArista("C", "D", 1);
+        g.agregarArista("D", "A", 1);
+
+        assertTrue(algorithms.puntosDeArticulacion(g).isEmpty());
+    }
+
+    @Test
+    public void puntosArticulacionInStarGraphTest(){
+        g.agregarVertice("A");
+        g.agregarVertice("B");
+        g.agregarVertice("C");
+        g.agregarVertice("D");
+        g.agregarVertice("E");
+        g.agregarArista("A", "B", 1);
+        g.agregarArista("A", "C", 1);
+        g.agregarArista("A", "D", 1);
+        g.agregarArista("A", "E", 1);
+        assertTrue(algorithms.puntosDeArticulacion(g).contains("A"));
+        assertEquals(1, algorithms.puntosDeArticulacion(g).size());
+    }
+
+    @Test
+    public void puntosArticulacionInDisconnectedGraphTest() {
+        g.agregarVertice("A");
+        g.agregarVertice("B");
+        g.agregarVertice("C");
+        g.agregarVertice("D");
+        g.agregarVertice("E");
+        g.agregarArista("A", "B", 1);
+        g.agregarArista("B", "C", 1);
+        g.agregarArista("D", "E", 1);
+
+        assertEquals(2, algorithms.puntosDeArticulacion(g).size());
+        assertTrue(algorithms.puntosDeArticulacion(g).contains("B"));
+        assertTrue(algorithms.puntosDeArticulacion(g).contains("D"));
     }
 }
